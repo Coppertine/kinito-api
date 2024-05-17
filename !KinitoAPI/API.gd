2aec04d208dbcb8c29952201127d02d2c4294a09
Coppertine <37494321+Coppertine@users.noreply.github.com>
2024-05-17 22:33:49 +1000

    Made API node based so mods have to register to use the api

--- !KinitoAPI/API.gd
+++ !KinitoAPI/API.gd
@@ -4,32 +4,31 @@ func _ready():
 	print("[api] API registered into App")
 	pass
 
-func _create_application(app_id):
-	_print("Creating application")
-	if typeof(app_id) != TYPE_INT:
-		_print("ERROR: Application " + str(app_id) + "is not an integer value.")
-		return
-	if get_tree().get_current_scene().get_node("Main").has_node(str(app_id)):
-		_print("ERROR: Application conflict found with: " + str(app_id))
-		_print("ERROR: Please change your application id, you can use an additional unique prefix in front of your application id.")
-		return
+func register_mod(mod_info):
+	_print("Registering new mod into api...")
+	if _setup_mod_check(mod_info):
+		_print("-------------------------------")
+		_print("ERROR: Mod will not be installed as there are errors found when registering to the API.")
+		return 1
 	
-	var node = Node2D.new()
-	node.name = str(app_id)
-	App.setNodeScale()
-	node.scale = Vars.get("nodeScale")
-	get_tree().get_current_scene().get_node("Main").add_child(node)
-	if Tab.data["open"].size() <= app_id:
-		Tab.data["open"].resize(app_id + 1)
-	if Tab.data["data"].size() <= app_id:
-		Tab.data["data"].resize(app_id + 1)
-	if Tab.data["pos"].size() <= app_id:
-		Tab.data["pos"].resize(app_id + 1)
-	Tab.data["open"][app_id] = false
-	Tab.data["data"][app_id] = 0
-	Tab.data["pos"][app_id] = Vector2(0,0)
-	_print("Created application: " + str(app_id))
+	# Compatability checks, need to add them into the mod_node
 	pass
 	
+func _setup_mod_check(mod_info):
+	var mod_error = false
+	if mod_info.mod_name == null or mod_info.mod_name == "":
+		_print("ERROR: mod_name is not defined. Please define a name for your mod to register with the API.")
+		mod_error = true
+	if mod_info.mod_author == null or mod_info.mod_author == "":
+		_print("ERROR: mod_author is not defined. Please define the author for your mod to register with the API.")		
+		mod_error = true
+	if mod_info.incompatable_mods != null:
+		var dir = Directory.new()
+		for incompatable_mod in mod_info.incompatable_mods:
+			if dir.file_exists("user://Mods/%s.zip" % incompatable_mod):
+				mod_error = true
+				OS.alert("Incompatable Mod:\nA mod found ('%s')" % incompatable_mod, mod_info.mod_name)
+				_print("ERROR: Incompatable mod found, '%s'. Please remove the mod to be able to run this mod with the API.")
+	return mod_error
 func _print(line):
 	print("[api] " + line)
